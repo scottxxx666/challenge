b205027782252cb1df0806a5baa9203679f60258
Scott Kao <scott.kao@kkday.com>
2018-03-29 04:00:39 +0800

    Staircase

---
+++ staircase.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the staircase function below.
+ */
+function staircase(n) {
+    for (let i = 1; i <= n; i++) {
+        let string = '';
+        for (let j = 1; j <= n; j++) {
+            if (j > (n - i)) {
+                string += '#';
+            } else {
+                string += ' ';
+            }
+        }
+        console.log(string);
+    }
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+    staircase(n);
+}
